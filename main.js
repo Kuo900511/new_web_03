435630695d338139e7c7d4ab5ba89d69799fb837
Kuo900511 <t112ac8012@ntut.org.tw>
2025-02-06 18:54:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let isAdDisplayed = false;
     window.addEventListener("scroll", () => {
       if (isAdDisplayed) return;
-      if (window.scrollY > 3000) {
+      if (window.scrollY > 2900) {
         adBanner.classList.add("active");
         isAdDisplayed = true;
       }
